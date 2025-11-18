f9bd2c38424e0341569005568f2d95e593acf40b
stp-ucn <1087710@ucn.dk>
2025-11-18 10:16:47 +0100

    MainMenu createTestData

--- CodeSharingExercise/src/tui/MainMenu.java
+++ CodeSharingExercise/src/tui/MainMenu.java
@@ -1,6 +1,8 @@
 package tui;
 import java.util.Scanner;
 
+import model.*;
+
 /**
  * MainMenu for Code sharing exercise
  * @author Anita Lykke Clemmensen
@@ -58,10 +60,31 @@ public class MainMenu {
     private void createTestData() {
     	//Only use classes from model layer
         //getInstance
+    	BookContainer bookCont = BookContainer.getInstance();
         //create some books
+    	Book blueJ = new Book("BlueJ", "Hansen");
+    	Book uml = new Book("UML", "Nielsen");
+    	Book up = new Book("UP", "Olsen");
+    	Book swot = new Book("SWOT", "Jensen");
         	//add to book container
+    		bookCont.addBook(blueJ);
+    		bookCont.addBook(uml);
+    		bookCont.addBook(up);
+    		bookCont.addBook(swot);
     	//create some copies
+    	BookCopy copyBJ1 = new BookCopy(101);
+    	BookCopy copyBJ2 = new BookCopy(102);
+    	BookCopy copyUP1 = new BookCopy(111);
+    	BookCopy copySWOT1 = new BookCopy(131);
+    	BookCopy copySWOT2 = new BookCopy(132);
+    	BookCopy copySWOT3 = new BookCopy(133);
     		//add to book
+        	blueJ.addBookCopy(101);
+        	blueJ.addBookCopy(102);
+        	up.addBookCopy(111);
+        	swot.addBookCopy(131);
+        	swot.addBookCopy(132);
+        	swot.addBookCopy(133);
     }
 
 	public static void main(String[] args) {
