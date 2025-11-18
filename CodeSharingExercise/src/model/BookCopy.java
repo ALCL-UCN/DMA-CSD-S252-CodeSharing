cc2e7c80dc73cc7048aec17ff0fa4827592a9d6c
stp-ucn <1087710@ucn.dk>
2025-11-18 10:14:36 +0100

    BookCopy implementation

---
+++ CodeSharingExercise/src/model/BookCopy.java
@@ -0,0 +1,16 @@
+package model;
+
+public class BookCopy {
+
+private int copyNumber;
+
+public BookCopy(int copyNumber) {
+	super();
+	this.copyNumber = copyNumber;
+}
+
+public int getCopyNumber() {
+	return copyNumber;
+}
+
+}
